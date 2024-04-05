140af1a3df2ecefcd1d3a064528387afc4657a47
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-04-05 22:41:02 +0200

    Add project files.

---
+++ Övning_3/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning_3
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+
+        public bool IsAdult
+        {
+            get { return (DateTime.Today.Year - BirthDate.Year) >= 18; }
+
+        }
+    }
+}
---
+++ Övning_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Övning_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Malte";
+            person.LastName = "Elwhagen";
+            person.BirthDate = new DateTime(2006, 1, 18);
+
+            Console.WriteLine($"Namn: {person.FullName}");
+            Console.WriteLine($"Vuxen: {person.IsAdult}");
+        }
+    }
+}
